9d5b0e79b00d2ec314e8c1b599e5fc878e73fffd
latemba <chalztemba@gmail.com>
2018-04-14 16:59:26 +0300

    routing to login page and default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-    {path:'login', component:LoginComponent}
+    {path:'login', component:LoginComponent},
+    { path: '',   redirectTo: '/login', pathMatch: 'full' },
+    {path: '***' , redirectTo: ''}
 ];
 @NgModule({
     imports:[RouterModule.forRoot(routes)],
