af8e4570e17662739634ae2a8f63a17b603ba766
latemba <chalztemba@gmail.com>
2018-04-15 13:54:10 +0300

    re arranged shared module importation

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared.module';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { SharedModule } from '../shared.module';
+
 
 @NgModule({
   imports: [
