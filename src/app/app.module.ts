c51a5207b817da2d0259660915d46e2c0260866c
latemba <chalztemba@gmail.com>
2018-04-22 13:16:23 +0300

    imported FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -15,6 +16,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
+    FormsModule,
     AdminModule,
     AppRoutingModule
   ],
