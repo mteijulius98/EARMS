a97aaca81ca810afd754c912e936a879f4e795e0
latemba <chalztemba@gmail.com>
2018-04-22 13:53:53 +0300

    moved header component to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { AuthguardService }            from './authguard.service';
 import { AuthService }          from './auth.service';
 @NgModule({
