746ddbbdc52604cd0e3a86b372a95b37db0f1200
mteijulius98 <mteijulius98@gmail.com>
2018-04-30 10:19:03 -0700

    Create user account

--- src/app/admin/addusers/addusers.component.ts
+++ src/app/admin/addusers/addusers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../../login/login-service.service';
 
 @Component({
   selector: 'app-addusers',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddusersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService:LoginServiceService) {  }
 
   ngOnInit() {
   }
-
+  ond(form:any){
+    
+    this.loginService.addUsers(form.value.firstname,form.value.lastname,form.value.email,form.value.password,form.value.station_name,form.value.role)
+    
+    .subscribe(
+      resp =>alert('Account created!!'),
+      error=>alert('error')
+    );
+   form.reset();
+  }
 }
--- src/app/login/login-service.service.ts
+++ src/app/login/login-service.service.ts
@@ -5,9 +5,21 @@ import { Observable} from 'rxjs';
 import 'rxjs/Rx';
 @Injectable()
 export class LoginServiceService {
-
+  public token=localStorage.getItem("token")
   constructor(private http:Http) { }
-  
+  addUsers(firstname:any, lastname:any,email:any, password:any,station_name:any,role:any){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization','jwt '+ this.token);
+    headers.append('X-Requested-With','XMLHttpRequest');
+    let options = {
+    headers: headers
+};
+let body = JSON.stringify({firstname: firstname, lastname:lastname,email:email, password:password,station_name:station_name,role:role});
+return this.http.post("http://localhost/fypyear/public/api/users", body, options).map( (response:Response) => response.json());
+   
+    
+ }
   signin(email: any, password:any){
     return this.http.post("http://localhost/fypyear/public/api/users/signin",
     {email:email, password:password},
