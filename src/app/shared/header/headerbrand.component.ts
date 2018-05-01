0828592f3a9c99a1ce66862a4274a79c0f4a8265
latemba <chalztemba@gmail.com>
2018-05-01 13:01:28 +0300

    redefined header component

---
+++ src/app/shared/header/headerbrand.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headerbrand',
+  templateUrl: './headerbrand.component.html',
+  styleUrls: ['./headerbrand.component.css']
+})
+export class HeaderbrandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
