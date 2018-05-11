758cf3f0387f7419ce593fc94a718e8c4394fb0f
latemba <chalztemba@gmail.com>
2018-05-11 17:35:48 +0300

    account drop down

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { HeaderbrandComponent } from './header/headerbrand.component';
 import { ToggleService } from './toggle.service';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   declarations: [
     HeaderComponent,
