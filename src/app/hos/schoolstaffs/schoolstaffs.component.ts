e29af330d0eb3c237149afafdeab3d27aebc0885
latemba <chalztemba@gmail.com>
2018-06-03 18:20:02 +0300

    renamed interface

--- src/app/hos/schoolstaffs/schoolstaffs.component.ts
+++ src/app/hos/schoolstaffs/schoolstaffs.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { HeadService } from '../head.service';
 
 @Component({
-  selector: 'app-schooldetails',
-  templateUrl: './schooldetails.component.html',
-  styleUrls: ['./schooldetails.component.css']
+  selector: 'app-schoolstaffs',
+  templateUrl: './schoolstaffs.component.html',
+  styleUrls: ['./schoolstaffs.component.css']
 })
-export class SchooldetailsComponent implements OnInit {
+export class SchoolstaffsComponent implements OnInit {
 
   constructor(private headService:HeadService) { }
 
