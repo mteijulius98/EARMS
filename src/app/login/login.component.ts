2ed136a700cbeeb09fd59469dd15619b5936a7e7
latemba <chalztemba@gmail.com>
2018-06-15 06:40:30 +0300

    added time services

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Router }      from '@angular/router';
 //import { AuthService } from '../shared/auth.service';
 import { LoginServiceService } from './login-service.service';
+import { TimeService } from '../shared/time.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+ currentYear:any;
   constructor(private loginService:LoginServiceService,private router:Router,
-     ) { }
+    private timeService:TimeService ) { }
   user={
 	email:'',
 	password:'',
@@ -21,7 +22,7 @@ private loginStatus:any;
   ngOnInit() {
     this.loginService.logout();
     //http call starts
-   
+    this.currentYear = this.timeService.showCurrentYear();
   }
   onSignin(){
       
@@ -40,5 +41,8 @@ private loginStatus:any;
      
     );
    
+  }
+  getYear() {
+    
   }
 }
