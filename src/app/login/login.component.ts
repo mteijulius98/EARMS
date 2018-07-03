9e6e4a4e34747d40b90941f65d77dcdd957edee5
latemba <chalztemba@gmail.com>
2018-07-03 13:50:05 +0300

    added spinner timeout on failure

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router }      from '@angular/router';
+import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 //import { AuthService } from '../shared/auth.service';
 import { LoginServiceService } from './login-service.service';
@@ -10,40 +10,39 @@ import { TimeService } from '../shared/time.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- currentYear:any;
-  constructor(private loginService:LoginServiceService,private router:Router,
-    private timeService:TimeService,private spinner:NgxSpinnerService ) { }
-  user={
-	email:'',
-	password:'',
-}
+  currentYear: any;
+  constructor(private loginService: LoginServiceService, private router: Router,
+    private timeService: TimeService, private spinner: NgxSpinnerService) { }
+  user = {
+    email: '',
+    password: '',
+  }
 
-private loginStatus:any;
+  private loginStatus: any;
 
   ngOnInit() {
     this.loginService.logout();
     //http call starts
     this.currentYear = this.timeService.showCurrentYear();
   }
-  onSignin(){
-      this.spinner.show();
+  onSignin() {
+    this.spinner.show();
     this.loginService.signin(this.user.email, this.user.password)
-    .subscribe(
+      .subscribe(
         response => {
           this.spinner.hide();
-            this.router.navigate([response.roinitial]);
-            
+          this.router.navigate([response.roinitial]);
+
         },
-        error=>{
-          this.spinner.hide();
-            alert("sorry u failed to login")
-            
-    
-          }
-       
-     
-    );
-   
+        error => {
+          setTimeout(() => {
+            /** spinner ends after 5 seconds */
+            this.spinner.hide();
+          }, 5000);
+          alert("sorry u failed to login")
+        }
+      );
+
   }
- 
+
 }
