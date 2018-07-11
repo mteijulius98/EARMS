f72f17e84cbad042d82b05d97e14ff167deecc58
latemba <chalztemba@gmail.com>
2018-07-11 16:03:35 +0300

    modified datatable

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit,OnDestroy,Input,Output,EventEmitter} from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
 import { AdminService } from '../admin.service';
 import { IUsers } from './users';
 import { LoginServiceService } from '../../login/login-service.service';
 import { Subject } from 'rxjs';
+import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit,OnDestroy {
-@Input() duser:IUsers;
-@Input() wuser:IUsers;
-@Output() userDeleted = new EventEmitter<IUsers>();
-dtOptions: DataTables.Settings = {};
-dtTrigger: Subject<any> = new Subject();
+export class UsersComponent implements AfterViewInit,OnInit, OnDestroy {
+  @Input() duser: IUsers;
+  @Input() wuser: IUsers;
+  @Output() userDeleted = new EventEmitter<IUsers>();
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
   role: string;
   users: IUsers[];
   errorMessage: string;
@@ -35,7 +38,6 @@ dtTrigger: Subject<any> = new Subject();
     this.adminService.viewRoles().subscribe(
       role => {
         this.roles = role.roles;
-        this.dtTrigger.next();
         console.log('our', role)
         //(districts:ourDistrict[]) => this.districts= districts,
       },
@@ -52,79 +54,89 @@ dtTrigger: Subject<any> = new Subject();
         this.wards = ward.wards
 
         //(districts:ourDistrict[]) => this.districts= districts,
-        },
-        error => this.errorMessage = <any>error);
-        this.adminService.viewSchools().subscribe(
-          school =>{
-          this.schools=school.schools
-         
-          //(districts:ourDistrict[]) => this.districts= districts,
-          },
-          error => this.errorMessage = <any>error);
-          this.adminService.viewDusers().subscribe(
-            duser =>{
-            this. dusers= duser.dusers;
-            this.dtTrigger.next();
-            console.log('our',duser)
-            //(districts:ourDistrict[]) => this.districts= districts,
-            },
-            error => this.errorMessage = <any>error);
-            this.adminService.viewWusers().subscribe(
-              wuser =>{
-              this. wusers= wuser.wusers
-              console.log('our',wuser)
-              //(districts:ourDistrict[]) => this.districts= districts,
-              },
-              error => this.errorMessage = <any>error);
-              this.adminService.viewSusers().subscribe(
-                suser =>{
-                this. susers= suser.susers
-                console.log('our',suser)
-                //(districts:ourDistrict[]) => this.districts= districts,
-                },
-                error => this.errorMessage = <any>error);
-               
-  // this.adminService.viewUsers().subscribe(
-  //     users => this.users = users,
-      
-  //     error => this.errorMessage = <any>error);
-      
-  
-} 
-ngOnDestroy(): void {
-  // Do not forget to unsubscribe the event
-  this.dtTrigger.unsubscribe();
-}
-onDelete(id){
-  console.log(id)
-  this.adminService.deleteUser(id)
-  .subscribe(
-    () => {
-       this.userDeleted.emit(id);
-      resp =>alert('User deleted');
-    }
-  );
-}
-form5(form:any){
-    
-  this.loginService.addUsers(form.value.firstname,form.value.middlename,form.value.lastname,form.value.email,form.value.password,form.value.role,form.value.station)
-  
-  .subscribe(
-    resp =>alert('Account created!!'),
-    error=>alert('error')
-  );
- form.reset();
-}
-form4(form:any){
-    
-  this.adminService.registerRole(form.value.role_name,form.value.role_initial)
-  
-  .subscribe(
-    resp =>alert('Role created!!'),
-    error=>alert('error')
-  );
- form.reset();
-}
+      },
+      error => this.errorMessage = <any>error);
+    this.adminService.viewSchools().subscribe(
+      school => {
+        this.schools = school.schools
 
-  
+        //(districts:ourDistrict[]) => this.districts= districts,
+      },
+      error => this.errorMessage = <any>error);
+    this.adminService.viewDusers().subscribe(
+      duser => {
+        this.dusers = duser.dusers;
+        console.log('our', duser)
+        //(districts:ourDistrict[]) => this.districts= districts,
+      },
+      error => this.errorMessage = <any>error);
+    this.adminService.viewWusers().subscribe(
+      wuser => {
+        this.wusers = wuser.wusers
+        console.log('our', wuser)
+        //(districts:ourDistrict[]) => this.districts= districts,
+      },
+      error => this.errorMessage = <any>error);
+    this.adminService.viewSusers().subscribe(
+      suser => {
+        this.susers = suser.susers
+        console.log('our', suser)
+        //(districts:ourDistrict[]) => this.districts= districts,
+      },
+      error => this.errorMessage = <any>error);
+
+    // this.adminService.viewUsers().subscribe(
+    //     users => this.users = users,
+
+    //     error => this.errorMessage = <any>error);
+
+
+  }
+  ngAfterViewInit(): void {
+    this.dtTrigger.next();
+  }
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+  // rerender(): void {
+  //   this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+  //     // Destroy the table first
+  //     dtInstance.destroy();
+  //     // Call the dtTrigger to rerender again
+  //     this.dtTrigger.next();
+  //   });
+  // }
+  onDelete(id) {
+    console.log(id)
+    this.adminService.deleteUser(id)
+      .subscribe(
+        () => {
+          this.userDeleted.emit(id);
+          resp => alert('User deleted');
+        }
+      );
+  }
+  form5(form: any) {
+
+    this.loginService.addUsers(form.value.firstname, form.value.middlename, form.value.lastname, form.value.email, form.value.password, form.value.role, form.value.station)
+
+      .subscribe(
+        resp => alert('Account created!!'),
+        error => alert('error')
+      );
+    form.reset();
+  }
+  form4(form: any) {
+
+    this.adminService.registerRole(form.value.role_name, form.value.role_initial)
+
+      .subscribe(
+        resp => alert('Role created!!'),
+        error => alert('error')
+      );
+    form.reset();
+  }
+
+
 }
