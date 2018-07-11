ff9b36dc9c25bf3b7150eeae35cc5591a8aa4742
latemba <chalztemba@gmail.com>
2018-07-11 13:54:39 +0300

    removed reports component

--- src/app/hos/hos-routing.module.ts
+++ src/app/hos/hos-routing.module.ts
@@ -6,7 +6,6 @@ import { HosComponent } from './hos.component';
 import { StaffsComponent } from './staffs/staffs.component';
 import { StudentsComponent } from './students/students.component';
 import { DocumentsComponent } from './documents/documents.component';
-import { ReportsComponent } from './reports/reports.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { FinancialsComponent } from './financials/financials.component';
 
@@ -23,7 +22,6 @@ const routes: Routes = [
           { path: 'staffs', component: StaffsComponent },
           { path: 'resources', component: ResourcesComponent },
           { path: 'financials', component: FinancialsComponent },
-          { path: 'reports', component: ReportsComponent },
           { path: 'documents', component: DocumentsComponent },
           { path: '', component: StudentsComponent}
         ]
--- src/app/hos/hos.module.ts
+++ src/app/hos/hos.module.ts
@@ -8,7 +8,6 @@ import { HosComponent } from './hos.component';
 import { StudentsComponent } from './students/students.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { StaffsComponent } from './staffs/staffs.component';
-import { ReportsComponent } from './reports/reports.component';
 import { HosService } from './hos.service';
 import { ResourcesComponent } from './resources/resources.component';
 import { FinancialsComponent } from './financials/financials.component';
@@ -26,7 +25,6 @@ import { FinancialsComponent } from './financials/financials.component';
     StudentsComponent,
     DocumentsComponent,
     StaffsComponent,
-    ReportsComponent,
     ResourcesComponent,
     FinancialsComponent
   ],
