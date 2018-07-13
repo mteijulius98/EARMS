a0ecf1f22ba77e540d349289cacb6ec5bb2e2b6d
latemba <chalztemba@gmail.com>
2018-07-13 16:01:51 +0300

    added reports to slo

--- src/app/slo/slo-routing.module.ts
+++ src/app/slo/slo-routing.module.ts
@@ -2,21 +2,36 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SloComponent } from './slo.component';
 import { ReportsComponent } from './reports/reports.component';
+import { WardslistComponent } from './reports/wardslist.component';
+import { WardsComponent } from './reports/wards.component';
+import { AuthGuard } from '../auth.guard';
 
 const routes: Routes = [
   {
-    path:'slo',
+    path:'deo',
     component:SloComponent,
     // canActivate:[AuthguardService],
     children:[
       {
         path: '',
         // canActivateChild: [AuthguardService],
-        children: [
-          {path:'',component:ReportsComponent}
+        children: [         
+          {path:'',component:ReportsComponent,
+          children:[
+            {
+              path: '',
+              // canActivateChild: [AuthguardService],
+              children: [
+                {path:'', component:WardslistComponent},
+                {path:'wardname', component:WardsComponent}
+              ]
+        }
         ]
       }
     ]
+  }
+],
+//canActivate: [AuthGuard],
   }
 ];
 
