0d9405e71c7fcbcfa9f3363141e41bb8d61689c2
latemba <chalztemba@gmail.com>
2018-07-19 17:59:48 +0300

    validated forms

--- src/app/admin/registerstations/registerstations.component.ts
+++ src/app/admin/registerstations/registerstations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../admin.service';
 import { Subject } from 'rxjs';
 
@@ -8,68 +8,80 @@ import { Subject } from 'rxjs';
   styleUrls: ['./registerstations.component.css']
 })
 export class RegisterstationsComponent implements OnInit {
-  regions=[];
-  districts=[];
-  errorMessage:string;
-  constructor(private adminService:AdminService) { 
-    setTimeout(function (){
-      $(function(){
+  region = {
+    name: '',
+  };
+  district = {
+    region: '',
+    name: '',
+    daddress: ''
+  };
+  ward = {
+    district: '',
+    name: '',
+    waddress: ''
+  };
+  regions = [];
+  districts = [];
+  errorMessage: string;
+  constructor(private adminService: AdminService) {
+    setTimeout(function () {
+      $(function () {
         $('#stations').DataTable({
-           pagingType: 'full_numbers',
-            pageLength: 5
-         });
+          pagingType: 'full_numbers',
+          pageLength: 5
         });
-         },2000);
+      });
+    }, 2000);
 
   }
 
   ngOnInit() {
-    
+
     this.adminService.viewRegions().subscribe(
-      region =>{
-      this.regions=region.regions
-      console.log('our',region)
-      //(districts:ourDistrict[]) => this.districts= districts,
+      region => {
+        this.regions = region.regions
+        console.log('our', region)
+        //(districts:ourDistrict[]) => this.districts= districts,
       },
       error => this.errorMessage = <any>error);
-      this.adminService.viewDistricts().subscribe(
-        district =>{
-        this.districts=district.districts
-        console.log('our',district)
+    this.adminService.viewDistricts().subscribe(
+      district => {
+        this.districts = district.districts
+        console.log('our', district)
         //(districts:ourDistrict[]) => this.districts= districts,
-        },
-        error => this.errorMessage = <any>error);
+      },
+      error => this.errorMessage = <any>error);
   }
 
-  form1(form:any){
-    
-    this.adminService.registerRegion(form.value.regionname)
-    
-    .subscribe(
-      resp =>alert('Region  created!!'),
-      error=>alert('error')
-    );
-   form.reset();
+  form1(form: any) {
+
+    this.adminService.registerRegion(this.region.name)
+      .subscribe(
+        resp => alert('Region  created!!'),
+        error => alert('error')
+      );
+    form.reset();
   }
-  form2(form:any){
-    
-    this.adminService.registerDistrict(form.value.districtname,form.value.region,form.value.postal_address)
-    
-    .subscribe(
-      resp =>alert('District  created!!'),
-      error=>alert('error')
-    );
-   form.reset();
+  form2(form: any) {
+
+    this.adminService.registerDistrict(this.district.name, this.district.region, this.district.daddress)
+
+      .subscribe(
+        resp => alert('District  created!!'),
+        error => alert('error')
+      );
+    form.reset();
   }
-  form3(form:any){
-    
-    this.adminService.registerWard(form.value.wardname,form.value.districtname,form.value.postal_address)
-    
-    .subscribe(
-      resp =>alert(' Ward  created!!'),
-      error=>alert('error')
-    );
-   form.reset();
+  form3(form: any) {
+
+    this.adminService.registerWard(this.ward.name, this.ward.district, this.ward.waddress)
+
+      .subscribe(
+        resp => alert(' Ward  created!!'),
+        error => alert('error')
+      );
+    form.reset();
   }
 
 
